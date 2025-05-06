d9e3e88d024e85804c88d40baa0c13c4b1c172ee
olzhas_nurman <olzhas.nurman@nu.edu.kz>
2025-05-06 23:45:26 +0500

    Feat: Added run stript that will run the tests.

---
+++ run.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+VERILATE_COMMAND_TRACE = "verilator --assert -I./rtl/ --Wall --trace --cc ./rtl/math_equation.sv --exe test/tb_math_equation.cpp"
+MAKE_COMMAND = "make -C obj_dir -f Vmath_equation.mk"
+SAVE_COMMAND = "./obj_dir/Vmath_equation"
+CLEAN_COMMAND = "rm -r ./obj_dir"
+
+def main():
+    os.system(VERILATE_COMMAND_TRACE)
+    os.system(MAKE_COMMAND)
+    os.system(SAVE_COMMAND)
+    os.system(CLEAN_COMMAND)
+
+
+
+main()
